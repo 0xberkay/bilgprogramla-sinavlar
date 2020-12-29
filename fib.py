c5d475b7de049878f4015c992db4eda760880fb8
Berkay Küçük <70816926+bksec@users.noreply.github.com>
2020-12-29 10:03:25 +0300

    renkler eklendi

--- fib.py
+++ fib.py
@@ -1,35 +1,36 @@
 # -*- coding: utf-8 -*-
 #fibonacci sayıları
 
-import os
-os.system('clear')# terminali temizedik
-terim_sayisi = int(input("""\033[34m
-... =============berkay=======[-][o][x]
+from renkler import * #renk modülünü import ediyoruz
+
+terim_sayisi = int(input("""
+...{} =============berkay.py====[-][o][x]
 ... |                                 |
-... |     fibonacci hesaplayıcısına   |
+... |    {} fibonacci hesaplayıcısına   |
 ... |           Hoş geldiniz          |
 ... |    kaç tane fibonacci terimi ?  |
-... |          bir değer girin        |
+... |          bir değer girin {} {}      |
 ... |                                 |
 ... |=================================|
-...   """))
+...   """.format(pembe,kirmizi,sifirla,mor))) #.formatla renkleri cagirdik
 
 ilk = 0
 ikinci = 1
 sayac = 0
 toplam = 0
-os.system('clear')# terminali temizedik
-print("""\033[31m
-... ============berkay========[-][o][x]
+print(f"""{turkuaz} 
+... ============berkay.py=====[-][o][x]
 ... |                                 |                    
-... |   girdiğiniz terim sayısı: {}   |
+... |   {lacivert}girdiğiniz terim sayısı: {terim_sayisi}{turkuaz}   |
 ... |                                 |
 ... |=================================|
-==>$""""\033[28m".format(terim_sayisi), end = " ")
+{yesil}==>$""", end = " ") # fstringle renkleri cagırdık
 while sayac < terim_sayisi:
-    print(ilk,end = " ") 
+    print(rastgeleRenkler(mavi,sari,yesil,kirmizi,beyaz,mor,pembe),ilk,end = " ") 
+    # ^^rastgele renkler belirleyip yazdirdik
     toplam = ilk + ikinci 
     ilk = ikinci 
     ikinci = toplam
     sayac+=1 
-print("")    
+print("")   
+
